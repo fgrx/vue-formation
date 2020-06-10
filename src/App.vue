d7f4518e98e19e060c1ffc4ed73b7434104c609a
unknown <fabien.grignoux@outlook.com>
2020-06-10 15:12:06 +0200

    partie 2 : mounted, computed props, création de composant

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,19 @@
 
     <v-content>
       <v-container>
+        <h1>Magasin de livres pour codeurs curieux</h1>
+
+        <h2>Top livres</h2>
         <v-row>
-          <v-col cols="6" sm="4" v-for="book in books" :key="book.id">
-            <v-card :color="book.top ? 'yellow lighten-4' : null" min-height="650px">
-              <v-img :src="book.image" :alt="'couverture du livre : ' + book.title "></v-img>
-              <v-card-title>{{ book.title }}</v-card-title>
-              <v-card-text>{{ book.description}}</v-card-text>
-              <v-card-actions>
-                <span v-if="book.quantity>0">Disponible</span>
-                <span v-else>Indisponible</span>
-              </v-card-actions>
-            </v-card>
+          <v-col cols="6" sm="4" v-for="book in topBooks" :key="book.id">
+            <BookItem :book="book" />
+          </v-col>
+        </v-row>
+
+        <h2>Tous les livres</h2>
+        <v-row>
+          <v-col cols="6" sm="4" v-for="book in classicBooks" :key="book.id">
+            <BookItem :book="book" />
           </v-col>
         </v-row>
       </v-container>
@@ -25,16 +27,35 @@
 </template>
 
 <script>
+import BookItem from "./components/BookItem.vue";
 import booksdb from "@/data/booksdb";
 
 export default {
   name: "App",
 
-  components: {},
+  components: { BookItem },
 
   data: () => ({
     books: booksdb
-  })
+  }),
+  computed: {
+    topBooks() {
+      // Version longue
+      // const topBooksFiltered = this.books.filter(function(book) {
+      //   if (book.top === true) {
+      //     return book;
+      //   }
+      // });
+
+      // return topBooksFiltered;
+
+      //version courte
+      return this.books.filter(book => book.top);
+    },
+    classicBooks() {
+      return this.books.filter(book => !book.top);
+    }
+  }
 };
 </script>
 
